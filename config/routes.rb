b1709b6b29c0009d39c833edd376ffffca19f207
Aske Bentzen <askebentzen@gmail.com>
2017-04-03 15:13:06 +0200

    added contact view/action/route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index' #start/default page
+
   get 'welcome/index'
   get 'welcome/about'
+  get 'welcome/contact'
 
-  root 'welcome#index' #start/default page
+
 end
