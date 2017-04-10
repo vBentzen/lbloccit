8a2563d4697db16d01eb62f7f3de8acda5f50774
Aske Bentzen <askebentzen@gmail.com>
2017-04-10 13:00:26 +0200

    finished assignment, not all correct

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 require 'random_data'
 
+#Create unique Post
+Post.find_or_create_by!(
+  title: "Unique Post",
+  body: "Unique Body"
+)
+poster = Post.first
 #Create Posts
 50.times do
   Post.create!(
@@ -17,6 +23,11 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+#Create a unique Comment
+Comment.find_or_create_by!(
+post: poster,
+body: "a super unique comment body"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
