5c404f4530516eb3cf7f396da548cc5c9cdef002
Aske Bentzen <askebentzen@gmail.com>
2017-04-14 16:14:36 +0200

    added  upate/destroy logic for posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -70,13 +70,61 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET edit" do
+    it "should return http success" do
+      get :edit, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
 
-  #  describe "GET edit" do
-  #    it "returns http success" do
-  #      get :edit
-  #      expect(response).to have_http_status(:success)
-  #    end
-  #  end
+    it "should render the #edit view" do
+      get :edit, {id: my_post.id}
+      expect(response).to render_template :edit
+    end
+
+    it "should assign post to be updated to @post" do
+      get :edit, {id: my_post.id}
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.body).to eq my_post.body
+    end
+  end
+
+  describe "PUT update" do
+    it "should update post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_post.id, post: {title: new_title, body: new_body}
+
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq my_post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+
+    it "should redirect to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_post.id, post: {title: new_title, body: new_body}
+      expect(response).to redirect_to my_post
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "should delete the post" do
+      delete :destroy, {id: my_post.id}
+      count = Post.where({id: my_post.id}).size
+      expect(count).to eq 0
+    end
+
+    it "should redirect to posts index" do
+      delete :destroy, {id: my_post.id}
+      expect(response).to redirect_to posts_path
+    end
+  end
 
 
 end
