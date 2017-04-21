8f7a06a4c44ca73f170da4987e98f54383910e9d
Aske Bentzen <askebentzen@gmail.com>
2017-04-21 15:38:48 +0200

    assignment finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     css_class << ' has-error' if errors.any?
     content_tag :div, capture(&block), class: css_class
   end
+
+  def render_byline_for(post)
+    "submitted #{time_ago_in_words(post.created_at)} ago by #{post.user.name} #{post.comments.count} Comments"
+  end
 end
