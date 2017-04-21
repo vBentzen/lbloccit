93af89754ac3612936f498255b733e915308cf7d
Aske Bentzen <askebentzen@gmail.com>
2017-04-21 14:47:41 +0200

    added logic so post belongs to user, all working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
-
   def show
     @post = Post.find(params[:id])
   end
@@ -11,11 +10,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
     @post.user = current_user
 
     if @post.save
@@ -33,8 +29,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -46,7 +41,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    git remove --cached @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
       redirect_to @post.topic
@@ -54,5 +49,10 @@ class PostsController < ApplicationController
       flash.now[:alert] = "There was an error deleting the post."
       render :show
     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
