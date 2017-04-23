78aa622f23704ad1d2e7e57b97c453178f60e6e5
Aske Bentzen <askebentzen@gmail.com>
2017-04-23 14:43:37 +0200

    assignment finished n' working, added feature to auto favorite created posts

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, cc: 'bentzenaske@icloud.com', subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<post/#{post.id}@bloccit.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@bloccit.com>"
+    headers["References"] = "<post/#{post.id}@bloccit.com>"
+
+    @post = post
+
+    mail(to: post.user.email, cc: 'bentzenaske@icloud.com', subject: "Your post #{post.id} have been created and is now being followed")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  after_create :add_to_favorites
 
   default_scope { order('rank DESC') }
 
@@ -28,5 +29,12 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def add_to_favorites
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end
